62471a3e90d7bfb27489329956a675447eb3a052
andrey_norvatov <whitesgn4@gmial.com>
2026-02-12 18:45:23 +0400

    Добавлено описание колонок в модели и файле миграции

--- src/example/models.py
+++ src/example/models.py
@@ -7,9 +7,9 @@ from src.model_mixins import TimestampMixin
 class Example(SQLModel, TimestampMixin, table=True):
     __tablename__ = "example"
 
-    id: int | None = Field(default=None, primary_key=True)
-    email: EmailStr = Field(index=True, unique=True, max_length=255)
-    name: str = Field(index=True, max_length=50)
-    full_name: str = Field(max_length=100)
-    hashed_password: str = Field(max_length=255)
-    is_active: bool = Field(default=True)
+    id: int | None = Field(default=None, primary_key=True, description="Уникальный идентификатор записи")
+    email: EmailStr = Field(index=True, unique=True, max_length=255, description="Электронная почта")
+    name: str = Field(index=True, max_length=50, description="Имя пользователя")
+    full_name: str = Field(max_length=100, description="Полное имя пользователя")
+    hashed_password: str = Field(max_length=255, description="Хэшированный пароль пользователя")
+    is_active: bool = Field(default=True, description="Статус активности пользователя")
